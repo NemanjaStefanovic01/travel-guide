de806fc6d2530be1f0b6e93be9d0dcf79354cdd2
MuseGt <nemanja.stefanovic3105@gmail.com>
2024-07-24 18:29:44 +0200

    Initial commit, creted app and defined font weight and size

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div>
+      <p className='font-h1'>MY APP</p>
+      <p className='font-h2'>MY APP</p>
+      <p className='font-h3'>MY APP</p>
+      <p className='font-subheading'>MY APP</p>
+      <p className='font-pharagraph'>My app</p>
+      <p className='font-smallText'>My app</p>
+    </div>
+  )
+}
+
+export default App
